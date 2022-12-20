83f2e0815962c37b32dd8d3a14730e978298891a
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2022-12-20 23:24:29 +0100

    add javascript to welcome page

--- assets/javascript/welcome.js
+++ assets/javascript/welcome.js
@@ -1,13 +1,6 @@
-const checkbox = document.getElementById("checkbox");
-const button = document.getElementById("button");
-button.addEventListener("clicked", function() {
-    if (checkbox.checked == 1) {
-        //proceed();
-        alert("checkbox");
-    }
-});
-
 function proceed() {
-    button.setAttribute(onclick, "location.href='question_page.html");
+    let ck = document.getElementById("checkbox").checked;
+    if (ck == 1) {
+        location.href = "question_page.html";
+    }
 }
-/* aggiungere javascript della prima pagina */
