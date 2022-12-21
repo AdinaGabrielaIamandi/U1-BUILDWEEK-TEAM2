92a5365bbdc30f9907d42533343f2b5d93a20e22
Nicola Di Quinzio <nicola.di.quinzio1@gmail.com>
2022-12-21 17:06:41 +0100

    add stars to reviewpage

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+const stars = document.querySelectorAll(".star");
+
+stars.forEach((star) => {
+  star.addEventListener("mouseover", () => {
+    const currentValue = parseInt(star.getAttribute("data-value"));
+    stars.forEach((s) => {
+      if (parseInt(s.getAttribute("data-value")) <= currentValue) {
+        s.classList.add("selected");
+      } else {
+        s.classList.remove("selected");
+      }
+    });
+  });
+
+  star.addEventListener("click", () => {
+    const currentValue = parseInt(star.getAttribute("data-value"));
+    stars.forEach((s) => {
+      s.classList.remove("selected");
+      if (parseInt(s.getAttribute("data-value")) <= currentValue) {
+        s.classList.add("selected");
+      }
+    });
+  });
+});
