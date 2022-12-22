08d575f1730e739ed2a601acb86921197f8474e3
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2022-12-22 16:01:20 +0100

    quiz js + timer js

--- assets/javascript/timer.js
+++ assets/javascript/timer.js
@@ -3,17 +3,17 @@ const WARNING_THRESHOLD = 10;
 const ALERT_THRESHOLD = 5;
 
 const COLOR_CODES = {
-  info: {
-    color: "green",
-  },
-  warning: {
-    color: "orange",
-    threshold: WARNING_THRESHOLD,
-  },
-  alert: {
-    color: "red",
-    threshold: ALERT_THRESHOLD,
-  },
+    info: {
+        color: "green",
+    },
+    warning: {
+        color: "orange",
+        threshold: WARNING_THRESHOLD,
+    },
+    alert: {
+        color: "red",
+        threshold: ALERT_THRESHOLD,
+    },
 };
 
 const TIME_LIMIT = 20;
@@ -50,64 +50,64 @@ document.getElementById("app").innerHTML = `
 startTimer();
 
 function onTimesUp() {
-  clearInterval(timerInterval);
+    clearInterval(timerInterval);
 }
 
 function startTimer() {
-  timerInterval = setInterval(() => {
-    timePassed = timePassed += 1;
-    timeLeft = TIME_LIMIT - timePassed;
-    document.getElementById("base-timer-label").innerHTML =
-      formatTime(timeLeft);
-    setCircleDasharray();
-    setRemainingPathColor(timeLeft);
+    timerInterval = setInterval(() => {
+        timePassed = timePassed += 1;
+        timeLeft = TIME_LIMIT - timePassed;
+        document.getElementById("base-timer-label").innerHTML =
+            formatTime(timeLeft);
+        setCircleDasharray();
+        setRemainingPathColor(timeLeft);
 
-    if (timeLeft === 0) {
-      onTimesUp();
-    }
-  }, 1000);
+        if (timeLeft === 0) {
+            onTimesUp();
+        }
+    }, 1000);
 }
 
 function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
 
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
 
-  return `${minutes}:${seconds}`;
+    return `${minutes}:${seconds}`;
 }
 
 function setRemainingPathColor(timeLeft) {
-  const { alert, warning, info } = COLOR_CODES;
-  if (timeLeft <= alert.threshold) {
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.remove(warning.color);
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.add(alert.color);
-  } else if (timeLeft <= warning.threshold) {
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.remove(info.color);
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.add(warning.color);
-  }
+    const { alert, warning, info } = COLOR_CODES;
+    if (timeLeft <= alert.threshold) {
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(warning.color);
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(alert.color);
+    } else if (timeLeft <= warning.threshold) {
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(info.color);
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(warning.color);
+    }
 }
 
 function calculateTimeFraction() {
-  const rawTimeFraction = timeLeft / TIME_LIMIT;
-  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    const rawTimeFraction = timeLeft / TIME_LIMIT;
+    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
 }
 
 function setCircleDasharray() {
-  const circleDasharray = `${(
+    const circleDasharray = `${(
     calculateTimeFraction() * FULL_DASH_ARRAY
   ).toFixed(0)} 283`;
-  document
-    .getElementById("base-timer-path-remaining")
-    .setAttribute("stroke-dasharray", circleDasharray);
+    document
+        .getElementById("base-timer-path-remaining")
+        .setAttribute("stroke-dasharray", circleDasharray);
 }
