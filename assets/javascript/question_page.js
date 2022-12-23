b0b82ee8a927d6eb50b7cfd729abed8a0e07e349
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2022-12-23 10:52:01 +0100

    Update question_page.js

--- assets/javascript/question_page.js
+++ assets/javascript/question_page.js
@@ -4,9 +4,9 @@ const WARNING_THRESHOLD = 10;
 const ALERT_THRESHOLD = 5;
 
 const COLOR_CODES = {
-  info: {
-    color: "green",
-  },
+    info: {
+        color: "green",
+    },
 };
 
 const TIME_LIMIT = 20;
@@ -42,169 +42,162 @@ document.getElementById("app").innerHTML = `
 startTimer();
 
 function resetTimer() {
-  timeLeft = TIME_LIMIT;
-  timePassed = 0;
-  clearInterval(timerInterval);
-  currentQuestion++; // incrementa la domanda corrente
-  startTimer();
+    timeLeft = TIME_LIMIT;
+    timePassed = 0;
+    clearInterval(timerInterval);
+    startTimer();
 }
 
 function startTimer() {
-  timerInterval = setInterval(() => {
-    timePassed = timePassed += 1;
-    timeLeft = TIME_LIMIT - timePassed;
-    document.getElementById("base-timer-label").innerHTML =
-      formatTime(timeLeft);
-    setCircleDasharray();
-    setRemainingPathColor(timeLeft);
+    timerInterval = setInterval(() => {
+        timePassed = timePassed += 1;
+        timeLeft = TIME_LIMIT - timePassed;
+        document.getElementById("base-timer-label").innerHTML =
+            formatTime(timeLeft);
+        setCircleDasharray();
+        setRemainingPathColor(timeLeft);
 
-    if (timeLeft === 0) {
-      showQuestion(currentQuestion); // mostra la domanda corrente
-    }
-  }, 1000);
+        if (timeLeft === 0) {
+            resetTimer();
+        }
+    }, 1000);
 }
 
 function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
 
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
 
-  return `${seconds}`;
+    return `${seconds}`;
 }
 
 function setRemainingPathColor(timeLeft) {
-  const { alert, warning, info } = COLOR_CODES;
-  if (timeLeft <= alert.threshold) {
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.remove(warning.color);
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.add(alert.color);
-  } else if (timeLeft <= warning.threshold) {
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.remove(info.color);
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.add(warning.color);
-  }
+    const { alert, warning, info } = COLOR_CODES;
+    if (timeLeft <= alert.threshold) {
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(warning.color);
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(alert.color);
+    } else if (timeLeft <= warning.threshold) {
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(info.color);
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(warning.color);
+    }
 }
 
 function calculateTimeFraction() {
-  const rawTimeFraction = timeLeft / TIME_LIMIT;
-  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    const rawTimeFraction = timeLeft / TIME_LIMIT;
+    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
 }
 
 function setCircleDasharray() {
-  const circleDasharray = `${(
+    const circleDasharray = `${(
     calculateTimeFraction() * FULL_DASH_ARRAY
   ).toFixed(0)} 283`;
-  document
-    .getElementById("base-timer-path-remaining")
-    .setAttribute("stroke-dasharray", circleDasharray);
+    document
+        .getElementById("base-timer-path-remaining")
+        .setAttribute("stroke-dasharray", circleDasharray);
 }
 
-window.onload = function () {};
+window.onload = function() {};
 
 //quiz
-const questions = [
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "What does CPU stand for?",
-    correct_answer: "Central Processing Unit",
-    incorrect_answers: [
-      "Central Process Unit",
-      "Computer Personal Unit",
-      "Central Processor Unit",
-    ],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
-    correct_answer: "Final",
-    incorrect_answers: ["Static", "Private", "Public"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "The logo for Snapchat is a Bell.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question:
-      "Pointers were not used in the original C programming language; they were added later on in C++.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "What is the most preferred image format used for logos in the Wikimedia database?",
-    correct_answer: ".svg",
-    incorrect_answers: [".png", ".jpeg", ".gif"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "In web design, what does CSS stand for?",
-    correct_answer: "Cascading Style Sheet",
-    incorrect_answers: [
-      "Counter Strike: Source",
-      "Corrective Style Sheet",
-      "Computer Style Sheet",
-    ],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "What is the code name for the mobile operating system Android 7.0?",
-    correct_answer: "Nougat",
-    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "On Twitter, what is the character limit for a Tweet?",
-    correct_answer: "140",
-    incorrect_answers: ["120", "160", "100"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "Linux was first created as an alternative to Windows XP.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "Which programming language shares its name with an island in Indonesia?",
-    correct_answer: "Java",
-    incorrect_answers: ["Python", "C", "Jakarta"],
-  },
+const questions = [{
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "What does CPU stand for?",
+        correct_answer: "Central Processing Unit",
+        incorrect_answers: [
+            "Central Process Unit",
+            "Computer Personal Unit",
+            "Central Processor Unit",
+        ],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+        correct_answer: "Final",
+        incorrect_answers: ["Static", "Private", "Public"],
+    },
+    {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "The logo for Snapchat is a Bell.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "Pointers were not used in the original C programming language; they were added later on in C++.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "What is the most preferred image format used for logos in the Wikimedia database?",
+        correct_answer: ".svg",
+        incorrect_answers: [".png", ".jpeg", ".gif"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "In web design, what does CSS stand for?",
+        correct_answer: "Cascading Style Sheet",
+        incorrect_answers: [
+            "Counter Strike: Source",
+            "Corrective Style Sheet",
+            "Computer Style Sheet",
+        ],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "What is the code name for the mobile operating system Android 7.0?",
+        correct_answer: "Nougat",
+        incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "On Twitter, what is the character limit for a Tweet?",
+        correct_answer: "140",
+        incorrect_answers: ["120", "160", "100"],
+    },
+    {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "Linux was first created as an alternative to Windows XP.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "Which programming language shares its name with an island in Indonesia?",
+        correct_answer: "Java",
+        incorrect_answers: ["Python", "C", "Jakarta"],
+    },
 ];
 
 let currentQuestion = 0;
@@ -212,57 +205,57 @@ let questionCounter = 1;
 let score = 0;
 
 function showQuestion() {
-  counter.innerText = `${questionCounter}`;
-  // recupera la domanda corrente
-  const question = questions[currentQuestion];
+    counter.innerText = `${questionCounter}`;
+    // recupera la domanda corrente
+    const question = questions[currentQuestion];
 
-  // mostra la domanda
-  document.querySelector("#question").innerHTML = question.question;
+    // mostra la domanda
+    document.querySelector("#question").innerHTML = question.question;
 
-  // mostra le risposte
-  let answersHTML = "";
-  for (const answer of question.incorrect_answers) {
-    answersHTML += `<button class="choice">${answer}</button>`;
-  }
-  answersHTML += `<button class="choice">${question.correct_answer}</button>`;
-  document.querySelector("#answers").innerHTML = answersHTML;
-
-  // imposta il timer di 20 secondi
-  timer = setTimeout(() => {
-    // passa alla domanda successiva
-    currentQuestion++;
-
-    if (currentQuestion < questions.length) {
-      showQuestion();
-      questionCounter++;
-      counter.innerText = `${questionCounter}`;
-    } else {
-      return window.location.assign(`../../Results03.html?result=${score}`);
+    // mostra le risposte
+    let answersHTML = "";
+    for (const answer of question.incorrect_answers) {
+        answersHTML += `<button class="choice">${answer}</button>`;
     }
-  }, 20000);
-  resetTimer();
+    answersHTML += `<button class="choice">${question.correct_answer}</button>`;
+    document.querySelector("#answers").innerHTML = answersHTML;
+
+    // imposta il timer di 20 secondi
+    timer = setTimeout(() => {
+        // passa alla domanda successiva
+        currentQuestion++;
+
+        if (currentQuestion < questions.length) {
+            showQuestion();
+            questionCounter++;
+            counter.innerText = `${questionCounter}`;
+        } else {
+            return window.location.assign(`../../Results03.html?result=${score}`);
+        }
+    }, 20000);
+    resetTimer();
 }
 
 document.querySelector("#answers").addEventListener("click", (event) => {
-  // verifica se l'utente ha selezionato la risposta corretta
-  if (event.target.innerHTML === questions[currentQuestion].correct_answer) {
-    score += 1;
-  }
+    // verifica se l'utente ha selezionato la risposta corretta
+    if (event.target.innerHTML === questions[currentQuestion].correct_answer) {
+        score += 1;
+    }
 
-  // passa alla domanda successiva
-  currentQuestion++;
+    // passa alla domanda successiva
+    currentQuestion++;
 
-  // cancella il timer quando l'utente seleziona una risposta
-  clearTimeout(timer);
+    // cancella il timer quando l'utente seleziona una risposta
+    clearTimeout(timer);
 
-  if (currentQuestion < questions.length) {
-    showQuestion();
-    questionCounter++;
-    counter.innerText = `${questionCounter}`;
-  } else {
-    return window.location.assign(`../../Results03.html?result=${score}`);
-  }
-  return score;
+    if (currentQuestion < questions.length) {
+        showQuestion();
+        questionCounter++;
+        counter.innerText = `${questionCounter}`;
+    } else {
+        return window.location.assign(`../../Results03.html?result=${score}`);
+    }
+    return score;
 });
 
 // mostra la prima domanda
